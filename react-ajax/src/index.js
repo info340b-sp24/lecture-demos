3bc6294a035fc9df237e27cf7c117f160eb6c253
Tim Carlson <timca@arenasports.net>
2024-05-21 10:18:20 -0700

    post ajax lecture

--- react-ajax/src/index.js
+++ react-ajax/src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
 
-import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import './index.css';
 
-import App from './components/App.js';
+// import App from './components/App.js';
 
 
-//render the root
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+// //render the root
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
 
 
 // // Slide 17
@@ -122,29 +122,29 @@ root.render(<App />);
 // root.render(<App />); 
 
 
-// //Slide 34
+//Slide 34
 
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-// import 'bootstrap/dist/css/bootstrap.css';
-// import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
 
-// import App from './components/App.js';
+import App from './components/App.js';
 
-// import 'whatwg-fetch' //load the polyfill we just installed via npm
+import 'whatwg-fetch' //load the polyfill we just installed via npm
 
-// const URL = "https://api.github.com/search/repositories?q=react&sort=stars";
+const URL = "https://api.github.com/search/repositories?q=react&sort=stars";
 
-// async function myAsyncFunction() {
+async function myAsyncFunction() {
   
-//   const response = await fetch(URL); //wait for fetch
-//   const data = await response.json(); //wait for encode
-//   console.log("data from the async method: ", data); //can use data ("synchronously")  
-// }
+  const response = await fetch(URL); //wait for fetch
+  const data = await response.json(); //wait for encode
+  console.log("data from the async method: ", data); //can use data ("synchronously")  
+}
 
-// myAsyncFunction();
+myAsyncFunction();
 
-// //render the root
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(<App />);
+//render the root
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
