7823af1f081a2348de2fa11f67581af74c1c28ce
Tim Carlson <timca@arenasports.net>
2024-05-23 10:35:16 -0700

    post firesbase db lecture

--- react-chat-firebase-1/src/index.js
+++ react-chat-firebase-1/src/index.js
@@ -8,9 +8,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
 import App from './components/App';
-import { initializeApp } from "firebase/app";
 // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,6 +17,7 @@ import { initializeApp } from "firebase/app";
 const firebaseConfig = {
   apiKey: "AIzaSyAYS3MDpTcEQnlREKtCREmzKe2NLHo7y0c",
   authDomain: "foo-delete-b5e24.firebaseapp.com",
+  databaseURL: "https://foo-delete-b5e24-default-rtdb.firebaseio.com",
   projectId: "foo-delete-b5e24",
   storageBucket: "foo-delete-b5e24.appspot.com",
   messagingSenderId: "217714342491",
@@ -25,8 +25,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-initializeApp(firebaseConfig);
-
+const app = initializeApp(firebaseConfig);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
